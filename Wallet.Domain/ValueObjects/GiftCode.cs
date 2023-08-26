e247aa91bd87f08e962e62e5e905313e97590267
Hamid N.CH <Hamidnch2007@Gmail.Com>
2023-08-26 23:37:30 +0330

    Added GiftCode and InstallmentWebsite as a ValueObject.

---
+++ Wallet.Domain/ValueObjects/GiftCode.cs
@@ -0,0 +1,15 @@
+﻿using Wallet.Domain.Common;
+
+namespace Wallet.Domain.ValueObjects;
+
+public class GiftCode : ValueObject<GiftCode>
+{
+    public string? Code { get; private set; }
+    public bool IsUsed { get; private set; }
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Code;
+        yield return IsUsed;
+    }
+}
---
+++ Wallet.Domain/ValueObjects/InstallmentWebsite.cs
@@ -0,0 +1,12 @@
+﻿using Wallet.Domain.Common;
+
+namespace Wallet.Domain.ValueObjects;
+
+public class InstallmentWebsite : ValueObject<InstallmentWebsite>
+{
+    public string? WebsiteUrl { get; private set; }
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return WebsiteUrl;
+    }
+}
